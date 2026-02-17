77f103a972f6cdda9f2e1e5bc02a4c5e4c7b4b7c
SAMUEL <samuellimabaixista@gmail.com>
2026-02-17 20:51:28 -0300

    Concluindo o projeto e a aula ( preparado para deploy no vercel)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { text } from 'express';
 
 const host='0.0.0.0.';
 const porta= 3000;
@@ -45,6 +45,67 @@ server.get('/horaAtual', (requisicao, resposta) => {
         
         `);
 
+});
+
+// criar um método que aceita parâmetros
+server.get('/ tabuada', (requisicao,resposta) => {
+
+    // tabuada de qual numero até qual sequencia?
+    console.log("requisicao tabuada");
+
+    const numero = parseInt (requisicao.query.numero);
+    const sequencia = parseInt(requisicao.query.sequencia);
+
+    if(!numero || !sequencia)
+       resposta.send(`
+        
+             <!DOCTYPE html>
+             <html>
+                <html lang="pt-br">
+                 <head>
+                 <meta charset="UTF-8">
+                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                 <title>TABUADA</title>
+             </head>
+             <body>
+                 <h1>Tabuada</h1>
+                 <h2>Por favor informe o numero e a sequencia na URL</h2>
+                 <h3>Exemplo: http://localhost:3000/tabuada?numero=5&sequencia=10</h3>
+              </body>
+              </html>
+        
+        `);
+
+    else{
+        resposta.setHeader('Content-Type','text/html');
+       
+        resposta.write (`<!DOCTYPE html>
+            <html>
+                
+             <head>
+                    <meta charset="UTF-8">
+                  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                 <title>TABUADA</title>
+             </head>
+                <body>
+                    <h1>Tabuada do ${numero} ate a sequencia ${sequencia}</h1>
+                    <ul>
+            
+                         `);
+
+                        for (let i =0; i<=sequencia;i++) {
+                             resposta.write(`<li>${i} x ${numero} = ${i*numero}</li>`);   
+                             }
+
+                         resposta.write(
+                    </ul>
+             </body>
+             </html>     
+
+         );
+
+        resposta.end() // FINALIZA E ENVIA
+    }
 });
 
 
